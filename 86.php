fbc0da4a1683cd5d3f9fd750c6e2ff4460685576
Radhesh Mutreja <119095313+Radhesh-Mutreja@users.noreply.github.com>
2025-04-15 08:40:34 +0530

    Update 86.php

--- 86.php
+++ 86.php
@@ -1,34 +1,29 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mydb";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// SQL to create table only if it doesn't already exist
-$sql = "CREATE TABLE IF NOT EXISTS students (
-    Stud_ID int(11) NOT NULL PRIMARY KEY,
-    Stud_Name varchar(40) NOT NULL,
-    Stud_Email varchar(40) NOT NULL,
-    Stud_Contact char(10) NOT NULL
-)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Table 'students' created successfully or already exists.<br>";
-} else {
-    echo "Error creating table: " . $conn->error . "<br>";
-}
-
-$conn->close();
-
-echo "This Program is written by Radhesh 0221BCA031";
-
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "mydb";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    
+    // sql to create table
+    $sql = "ALTER TABLE students
+    ADD PRIMARY KEY (Stud_ID),
+    ADD UNIQUE KEY Stud_Email (Stud_Email);";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Table students altered successfully";
+    } else {
+        echo "Error altering table: " . $conn->error;
+    }
+    
+    $conn->close();
+   echo"This Program is written by Radhesh 0221BCA031";
+        
 ?>
